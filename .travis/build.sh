243c5fb49e68c6f031f4bb25ffd4c11df0118f0d
Jeffrey Zhang <zhang.lei.fly@gmail.com>
2018-08-04 23:48:09 +0800

    use travis env to control image version to build

--- .travis/build.sh
+++ .travis/build.sh
@@ -3,8 +3,6 @@
 set -e
 set -u
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-pushd node_exporter
-make push VERSION=0.16.0
-make push VERSION=0.15.2
-make push VERSION=0.14.0
+pushd $APP
+make push
 popd
