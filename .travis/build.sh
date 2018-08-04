c4cb9930ae423ce755515dbb06e1caabe3fe7c2c
Jeffrey Zhang <zhang.lei.fly@gmail.com>
2018-08-05 00:28:23 +0800

    support latest tag

--- .travis/build.sh
+++ .travis/build.sh
@@ -4,5 +4,12 @@ set -e
 set -u
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 pushd $APP
-make push
+MAKE_OPTS="IMAGE=${DOCKER_USERNAME}/${APP}"
+if [[ -n ${VERSION+x} ]]; then
+    MAKE_OPTS="${MAKE_OPTS} VERSIO=${VERSION}"
+fi
+if [[ -n ${LATEST+x} ]]; then
+    MAKE_OPTS="${MAKE_OPTS} LATEST=1"
+fi
+make push ${MAKE_OPTS}
 popd
