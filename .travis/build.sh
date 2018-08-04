0200fdb6f79b6dd67258ad0948928ac0614f1f09
Jeffrey Zhang <zhang.lei.fly@gmail.com>
2018-08-05 00:31:10 +0800

    fix typo

--- .travis/build.sh
+++ .travis/build.sh
@@ -6,7 +6,7 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 pushd $APP
 MAKE_OPTS="IMAGE=${DOCKER_USERNAME}/${APP}"
 if [[ -n ${VERSION+x} ]]; then
-    MAKE_OPTS="${MAKE_OPTS} VERSIO=${VERSION}"
+    MAKE_OPTS="${MAKE_OPTS} VERSION=${VERSION}"
 fi
 if [[ -n ${LATEST+x} ]]; then
     MAKE_OPTS="${MAKE_OPTS} LATEST=1"
