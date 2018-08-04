26602f3c580389baa7763b15996f668992b383c4
Jeffrey Zhang <zhang.lei.fly@gmail.com>
2018-08-04 23:28:54 +0800

    build 0.14.0/0.15.2/0.16.0 image

--- build.sh
+++ build.sh
@@ -4,5 +4,7 @@ set -e
 set -u
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 pushd node_exporter
-make push
+make push VERSION=0.16.0
+make push VERSION=0.15.2
+make push VERSION=0.14.0
 popd
